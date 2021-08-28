f372c16e77c7f8459b5f028542ea6ed8bc2c3536
Kamalakannan R <rkamalakannan>
2021-08-28 12:38:27 +0000

    Initial commit

---
+++ src/main/java/com/sangamal/Application.java
@@ -0,0 +1,10 @@
+package com.sangamal;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
